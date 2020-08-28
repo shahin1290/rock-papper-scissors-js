dd4bae0c68b54cf2e7df184c9aad470f6d4f8874
shahin1290 <s.patowary@yahoo.com>
2020-08-29 00:14:47 +0200

    Adds player choice function

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
+const ROCK = 'ROCK'
+const PAPPER = 'PAPPER'
+const SCISSORS = "SCISSORS"
+const DEFAULT_SELECTION = 'ROCK'
 
-  startGameBtn.addEventListener('click', function(){
-    console.log('Game is starting')
-  })
+let gameIsRunning = false
+
+const getPlayerChoice = function(){
+  const selection = prompt(`${ROCK} or ${PAPPER} or ${SCISSORS}`, '').toUpperCase()
+  if(selection !== ROCK && selection !== PAPPER && selection !== SCISSORS){
+    alert(`invalid selection! and ${DEFAULT_SELECTION} is selected by default `)
+    return DEFAULT_SELECTION
+  }else {
+    return selection
+  }
+}
+
+
+const selection = startGameBtn.addEventListener('click', function(){
+  if(gameIsRunning){
+    return
+  }
+  gameIsRunning = true
+  console.log('Game is starting')
+  const playerChoice = getPlayerChoice()
+  console.log(playerChoice)
+})
+
+
+
 
