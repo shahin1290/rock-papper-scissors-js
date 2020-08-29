0443c24cfb8e0b1441620e252797cff878fd4d0d
shahin1290 <s.patowary@yahoo.com>
2020-08-29 19:59:45 +0200

    Adds game logic

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
 const ROCK = 'ROCK'
-const PAPPER = 'PAPPER'
-const SCISSORS = "SCISSORS"
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
 const DEFAULT_SELECTION = 'ROCK'
+const RESULT_DRAW = 'DRAW'
+const RESULT_PLAYER_WIN = 'YOU WIN'
+const RESULT_COMPUTER_WIN = 'COMPUTER WINS'
 
 let gameIsRunning = false
 
-const getPlayerChoice = function(){
-  const selection = prompt(`${ROCK} or ${PAPPER} or ${SCISSORS}`, '').toUpperCase()
-  if(selection !== ROCK && selection !== PAPPER && selection !== SCISSORS){
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK} or ${PAPER} or ${SCISSORS}`,
+    ''
+  ).toUpperCase()
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`invalid selection! and ${DEFAULT_SELECTION} is selected by default `)
     return DEFAULT_SELECTION
-  }else {
+  } else {
     return selection
   }
 }
 
+const getComputerChoice = () => {
+  const randomNumber = Math.random()
+  if (randomNumber < 0.34) {
+    return ROCK
+  } else if (randomNumber < 0.64) {
+    return PAPER
+  } else {
+    return SCISSORS
+  }
+}
 
-const selection = startGameBtn.addEventListener('click', function(){
-  if(gameIsRunning){
+const getResult = (pChoice, cChoice) =>
+  pChoice === cChoice
+    ? RESULT_DRAW
+    : (pChoice === ROCK && cChoice === SCISSORS) ||
+      (pChoice === PAPER && cChoice === ROCK) ||
+      (pChoice === SCISSORS && cChoice === PAPER)
+    ? RESULT_PLAYER_WIN
+    : RESULT_COMPUTER_WIN
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
     return
   }
   gameIsRunning = true
   console.log('Game is starting')
-  const playerChoice = getPlayerChoice()
-  console.log(playerChoice)
+  const pChoice = getPlayerChoice()
+  const cChoice = getComputerChoice()
+  console.log(pChoice)
+  console.log(cChoice)
+  const result = getResult(pChoice, cChoice)
+  console.log(result)
 })
-
-
-
-
